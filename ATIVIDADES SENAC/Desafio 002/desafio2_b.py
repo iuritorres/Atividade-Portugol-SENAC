1619ac366853ac70941349d4a1b48a26d58f8cc6
Rafael Luis Barreto Silva <98782405+rafaelluiis2315@users.noreply.github.com>
2022-05-10 15:58:27 -0300

    Código funcionando

--- ATIVIDADES SENAC/Desafio 002/desafio2_b.py
+++ ATIVIDADES SENAC/Desafio 002/desafio2_b.py
@@ -1,25 +1,66 @@
-### DESENVOLVEDORES : IURI TORRES | RAFAEL LUÍS ###
+### DESENVOLVEDORES : RAFAEL LUÍS | IURI TORRES ###
 
 # IMPORTAÇÕES
 
+from ast import While
+from operator import index
+import os
+from time import sleep
+import datetime
+
+from numpy import true_divide
 
 # CÓDIGO
+lista_nome = []
+lista_idade = []
+lista_sexo = []
 
-nome1 = input('Qual o seu nome? ')                                # Armazena nome1
-idade1 = int(input('Qual a sua idade? '))                         # Armazena idade1
-sexo1 = input('Qual o seu sexo? (M/F): ')                         # Armazena sexo1
+nome = ''
+idade = ''
+sexo = ''
 
-nome2 = input('Qual o seu nome? ')                                # Armazena nome2
-idade2 = int(input('Qual a sua idade? '))                         # Armazena idade2
-sexo2 = input('Qual o seu sexo? (M/F): ')                         # Armazena sexo2
+while True:
+    print('\n','='*57,'\n')                                              # DIVISÓRIA
+
+    print(' Digite 0 nos 3 campos para parar a consulta!\n')             # DIVISÓRIA
+
+    if nome != '0':
+                  
+
+        nome = input(' Qual o seu nome? ').upper()                                   # Armazena nome
+
+        lista_nome.append(nome)
+
+        
+        idade = int(input(' Qual a sua idade? '))                             # Armazena idade
+
+        lista_idade.append(idade)
+
+        sexo = str(input(' Qual o seu sexo? (M/F): ')).upper()                # Armazena sexo1
+
+        if sexo[0] == 'M':                                                   # valição do sexo
+            sexo = 'M'
+
+        elif sexo[0] == 'F':
+            sexo = 'F'
+
+        lista_sexo.append(sexo)                                             # Fechamento da validção
+    elif nome == '0':
+
+        break                                                  
+
+os.system('cls')
+
+print('\n','='*57,'\n')   # DIVISÓRIA
 
 # CONDIÇÃO DE EXIBIÇÃO DE NOME (SEXO MASCULINO COM MAIS DE 21 ANOS)
+for nome in lista_nome:
+    posicao_nome = lista_nome.index(nome)
 
-if idade1 and idade2 > 21 and sexo1 == 'M' and sexo2 == 'M':
-    print('Nome: \n\n - {} \n - {}'.format(nome1, nome2))
+    nome = lista_nome[posicao_nome]
+    idade = lista_idade[posicao_nome]
+    sexo = lista_sexo[posicao_nome] 
 
-elif idade1 > 21 and sexo1 != 'F':
-    print('Nome: \n\n - {}'.format(nome1))
-
-elif idade2 > 21 and sexo2 != 'F':
-    print('Nome: \n\n - {}'.format(nome2))
+    if idade > 21 and sexo == 'M' :
+        
+        print('Nome: \n\n - {} '.format(nome))
